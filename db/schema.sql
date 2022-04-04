279d82356ce90e1d3b0627bdd97d28650d906d19
Tyler <tylersundquist3@gmail.com>
2022-04-04 02:05:11 -0600

    reorganized schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -2,15 +2,9 @@ DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS department;
 
-
-CREATE TABLE employee (
- id INTEGER AUTO_INCREMENT PRIMARY KEY,
- first_name VARCHAR(30),
- last_name VARCHAR(30),
- role_id INTEGER,
- CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
- manager_id INTEGER,
- CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30)
 );
 
 CREATE TABLE role (
@@ -21,7 +15,12 @@ CREATE TABLE role (
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE department (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30)
+CREATE TABLE employee (
+ id INTEGER AUTO_INCREMENT PRIMARY KEY,
+ first_name VARCHAR(30),
+ last_name VARCHAR(30),
+ role_id INTEGER,
+ CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+ manager_id INTEGER,
+ CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,21 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO department (department_name)
 VALUES
-('James', 'Smith', 1, 1),
-('Alexis', 'Sondra', 2, 2),
-('Alysa', 'Sullivan', 3, 3),
-('Zach', 'Albert', 4, 4),
-('Oakley', 'Weston', 1, 1)
+('Sales'),
+('Engineering'),
+('Science'),
+('Boss');
 
 INSERT INTO role (role_title, role_salary, department_id)
 VALUES
 ('Salespeople', 60000, 1),
 ('Engineer', 100000, 2),
 ('Chemist', 140000, 3),
-('CEO', 300000, 4)
+('CEO', 300000, 4);
 
-INSERT INTO department (department_name)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Sales'),
-('Engineering'),
-('Science'),
-('Boss'),
+('James', 'Smith', 1, 1),
+('Alexis', 'Sondra', 2, 2),
+('Alysa', 'Sullivan', 3, 3),
+('Zach', 'Albert', 4, 4),
+('Oakley', 'Weston', 1, 1);
